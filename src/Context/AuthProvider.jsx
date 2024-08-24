5cb6aa94ccd86de0cfc78378ccb94a620a56d626
sabbir ahmed <sabbirahmed.info33@gmail.com>
2024-08-24 16:47:12 +0600

    my first commit

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -2,6 +2,9 @@ import { createContext } from "react";
 
 export const authContext = createContext(null);
 const AuthProvider = ({ children }) => {
+
+
+  
   return <authContext.Provider>{children}</authContext.Provider>;
 };
 
---
+++ src/Hooks/useAuth.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react"
+import { authContext } from "../Context/AuthProvider"
+
+
+const useAuth = () => {
+    const auth = useContext(authContext)
+  return auth
+}
+
+export default useAuth
